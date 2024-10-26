fdc5f0d1c7ef0c20a9c8740b0fad19d10a094dbf
Alessandro Viana <alessandroviana830@gmail.com>
2024-10-26 10:35:07 -0300

    Colocando fonte do Google Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello,World</h1>
+      <h1 className="title">Hello World</h1>
       <GlobalStyle />
     </div>
   );
