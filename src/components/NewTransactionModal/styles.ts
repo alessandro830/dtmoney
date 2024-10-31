ac1d256cf64943ee68a1352b3d7d52f980a8e5db
Alessandro Viana <alessandroviana830@gmail.com>
2024-10-31 16:09:26 -0300

    Funcionamento dos Botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -54,33 +54,36 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`
+interface RadioBoxProps {
+    isActive:boolean;
+}
 
-    button {
-        height: 4rem;
-        border: 1px solid #D7D7D7;
-        border-radius: 0.25rem;
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #D7D7D7;
+    border-radius: 0.25rem;
 
-        background: transparent;
+    background: ${(props) => props.isActive ? '#CCC' : 'transparent'};
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transition: border-color 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: border-color 0.2s;
 
-        &:hover {
-            border-color: ${darken(0.1, '#D7D7D7')};
-        }
+    &:hover {
+        border-color: ${darken(0.1, '#D7D7D7')};
+    }
 
-        img {
-            width: 20px;
-            height: 20px;
-        }
+    img {
+        width: 20px;
+        height: 20px;
+    }
 
-        span {
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
     }
 `
